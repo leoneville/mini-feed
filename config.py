1d7e403225e17600e28119a6986cb6ddb3955a87
leoneville <leoneville_@hotmail.com>
2023-05-10 12:59:43 -0300

    feat: add database connection and create model user

--- config.py
+++ config.py
@@ -6,6 +6,12 @@ load_dotenv()
 class Config:
 
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    USERNAME = os.environ.get("MYSQL_ROOT_PASSWORD")
+    PASSWORD = os.environ.get("MYSQL_PASSWORD")
+    DATABASE = os.environ.get("MYSQL_DATABASE")
+
+    SQLALCHEMY_DATABASE_URI = f"mysql://{USERNAME}:{PASSWORD}@localhost/{DATABASE}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     APP_TITLE = "Flask REST API"
 
